8f4f3a10fc0a54be506bec2d5dd9582550a016c9
RizqiZamzamijamil <rizqizamzamij@gmail.com>
2022-04-26 23:28:53 +0700

    Submit Jobsheet M10 Desain dan Pemrograman Web

---
+++ homeCRUD.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>DATA MAHASISWA</h1>
+    <form method="post" action="insertForm.html">
+      <button type="submit">Tambah Data</button>
+     </form>
+    <table>
+        <tr>
+            <th> ID </th>
+            <th> Foto </th>
+            <th> Nama </th>
+            <th> Alamat</th>
+            <th> Aksi </th>
+        </tr>
+        <?php
+            include "myconnection.php";
+
+            $query = "SELECT * FROM student";
+            $result = mysqli_query($connect, $query);
+
+            if(mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_array($result)) {
+        ?>
+        <tr>
+            <td> <?php echo $row["id"]; ?> </td>
+            <td> <img width=200 src="gambar/<?php echo $row["foto"]; ?>" alt=""> </td>
+            <td> <?php echo $row["name"]; ?> </td>
+            <td> <?php echo $row["address"]; ?> </td>
+
+            <td> 
+                <a href="editForm.php?id=<?php echo $row["id"]; ?>"> 
+                <button>Edit</button> </a>
+                <a href="delete.php?id=<?php echo $row["id"]; ?>"> 
+                <button>Hapus</button> </a>  
+            </td>
+        </tr>
+        <?php
+                }
+            } else {
+                echo "0 results";
+            }
+        ?>
+    </table>
+</body>
+</html>
